026f20bff8ada1bc194969d4433eb38ee1fc813a
theodorgto <66820813+theodorgto@users.noreply.github.com>
2020-11-10 16:30:18 +0100

    udvidet switch table

--- finasgmt/src/main/scala/SingleCycleRiscV.scala
+++ finasgmt/src/main/scala/SingleCycleRiscV.scala
@@ -37,12 +37,90 @@ class SingleCycleRiscV extends Module {
   val opcode = instr(6, 0)
   val rd = instr(11, 7)
   val rs1 = instr(19, 15)
+  val rs2 = instr(24, 20)
+  val funct3 = instr(14, 12)
+  val funct7 = instr(31 ,25)
   val imm = instr(31, 20) // TODO sign extend
 
   switch(opcode) {
-    is(0x13.U) {
-      reg(rd) := reg(rs1) + imm
+    is(0x33.U) {                              //R-type
+      switch(funct3) {
+        is(0x0.U) {  //add, sub
+          switch(funct7) {
+            is(0x00.U) {
+              reg(rd) := reg(rs1) + reg(rs2) //add
+            }
+            is(0x20.U) {
+              reg(rd) := reg(rs1) - reg(rs2) //sub
+            }
+          }
+        }
+        is(0x6.U) {
+          reg(rd) := reg(rs1) | reg(rs2) //or
+        }
+        is(0x7.U) {
+          reg(rd) := reg(rs1) & reg(rs2) //and
+        }
+        is(0x1.U) {
+          reg(rd) := reg(rs1) << reg(rs2) //sll
+        }
+        is(0x5.U) {
+          switch(funct7) {
+            is(0x00.U) {
+              reg(rd) := reg(rs1) >> reg(rs2) //srl
+            }
+            is(0x20.U) {
+              reg(rd) := reg(rs1) >> reg(rs2) //sra ***** mangler msb-extend
+            }
+          }
+        }
+      }
+      reg(rd) := reg(rs1) + reg(rs2) //add
     }
+    is(0x13.U) {                              //I-type
+      switch(funct3) {
+        is(0x0.U) {
+          reg(rd) := reg(rs1) + imm //addi
+        }
+        is(0x4.U) {
+          reg(rd) := reg(rs1) ^ imm //xori
+        }
+        is(0x6.U) {
+          reg(rd) := reg(rs1) | imm //ori
+        }
+        is(0x7.U) {
+          reg(rd) := reg(rs1) & imm //andi
+        }
+        is(0x1.U) {
+          reg(rd) := reg(rs1) << imm(0, 4) //slli
+        }
+        is(0x5.U) {
+          switch(imm(5, 11)) {
+            is(0x00.U) {
+              reg(rd) := reg(rs1) >> imm(0, 4) //srli
+            }
+            is(0x20.U) {
+              reg(rd) := reg(rs1) >> imm(0, 4) //srai ***** mangler msb-extend
+            }
+          }
+        }
+        is(0x2.U) {
+          reg(rd) := reg(rs1) < imm //slti ****** virker måske ikke?
+        }
+        is(0x3.U) {
+          reg(rd) := reg(rs1) < imm //sltiu ****** virker måske ikke? mangler zero-extend
+        }
+      }
+    }
+    is(0x03.U) {
+      switch(funct3) {
+        is(0x0.U) {
+          reg(rd) := reg(rs1) + imm //lb ****** implement memory
+        }
+    }
+    is(0x23.U) {                              //S-type
+    }
+
   }
 
   pc := pc + 4.U
