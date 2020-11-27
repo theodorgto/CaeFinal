513b8069bbfc8ad99eac04f80f67b272d1a3dc62
leouhre <leouhrejakobsen@gmail.com>
2020-11-27 12:57:04 +0100

    further development

--- finasgmt/src/main/scala/SingleCycleRiscV.scala
+++ finasgmt/src/main/scala/SingleCycleRiscV.scala
@@ -8,36 +8,31 @@ import chisel3.util._
   */
 class SingleCycleRiscV extends Module {
   val io = IO(new Bundle {
-    val regDeb = Output(Vec(32, UInt(32.W))) // debug output for the tester
+    val regDeb = Output(Vec(32, UInt(32.W))) //debug output for the tester
     val done = Output(Bool())
-    val imemDeb = Output(Vec(109, SInt(32.W)))
-    val pcDeb = Output(UInt(32.W))
-    val memDeb = Output(UInt(32.W))
+    val imemDeb = Output(Vec(109, SInt(32.W))) //debug output for the tester
+    val pcDeb = Output(UInt(32.W)) //debug output for the tester
+    //val memDeb = Output(UInt(32.W)) //debug output for the tester
   })
 
-
+  //necessary io initializations
+  io.done := false.B
   for (i <- 0 until 109) io.imemDeb(i) := 0.S
 
-  val program = CopyBytes("tests/task3/loop.bin") //load machine code from file
-  val imem = VecInit(program.map(_.S(32.W)))  //map to chisel vec
-  for (i <- program.indices) io.imemDeb(i) := imem(i) //for the tester
-
+  //load machine code from binary file using CopyBytes
+  val program = CopyBytes("tests/task3/loop.bin")
+  val imem = VecInit(program.map(_.S(32.W)))  //map into instruction memory as chisel vec
 
 /*
   val program = Array[BigInt](
-    0x00400513,
-    0x00200593,
-    0x00b52223,
-    0x00452603)
+    0x00a00513,
+    0x80000593,
+    0x4025d593)
 
   // A little bit of functional magic to convert the Scala Int Array to a Chisel Vec of UInt
   val imem = VecInit(program.map(_.S(32.W)))
-  for (i <- program.indices) io.imemDeb(i) := imem(i) //for the tester
-
  */
 
-
-  val pc = RegInit(0.U(32.W))
 
   // TODO: there should be an elegant way to express this
   val vec = Wire(Vec(32, UInt(32.W)))
@@ -45,24 +40,25 @@ class SingleCycleRiscV extends Module {
   // We initialize the register file to 0 for a nicer display
   // In a real processor this is usually not done
   val reg = RegInit(vec)
-  reg(0) := 0.U
 
-  val instr = imem(pc(31, 2))
-
+  val pc = RegInit(0.U(32.W))
+  val instr = imem(pc(31, 2)) //byte offset
   val opcode = instr(6, 0)
   val rd = instr(11, 7)
   val rs1 = instr(19, 15)
   val rs2 = instr(24, 20)
   val funct3 = instr(14, 12)
   val funct7 = instr(31 ,25)
+  //immediates are first defined as words
   val imm = WireInit(0.U (32.W))
   val Bimm = WireInit(0.U (32.W))
   val Simm = WireInit(0.U (32.W))
   val Jimm = WireInit(0.U (32.W))
   val Uimm = WireInit(0.U (32.W))
-  Uimm := instr(31, 12) << 12
+  Uimm := instr(31, 12) << 12 //Uimm(11, 0) = 0x00
   val sign = instr(31)
-  when (sign) {                           //sign extension
+  val sraisign = reg(rs1)
+  when (sign) { //sign extension of immediates
     imm := Cat(0xFFFFF.U, instr(31, 20))
     Bimm := Cat(0xFFFFF.U, instr(31),instr(7),instr(30,25),instr(11,8),"b0".U)
     Simm := Cat(0xFFFFF.U, instr(31,25),instr(11,7))
@@ -73,7 +69,6 @@ class SingleCycleRiscV extends Module {
     Simm := Cat(instr(31,25),instr(11,7))
     Jimm := Cat(instr(31), instr(19, 12), instr(20), instr(30, 21), "b0".U)
   }
-  val sraisign = reg(rs1)
   val shift = WireInit(0.U (5.W)) //kan måske ændres til reg(rs2) direkte
 
   //memory - 1MB
@@ -84,113 +79,114 @@ class SingleCycleRiscV extends Module {
 
   pc := pc + 4.U
 
+  //Decoding of instruction and execution
   switch(opcode) {
-    is(0x33.U) {                              //R-type
+    //____________________________________R-type_________________________
+    is(0x33.U) {
       switch(funct3) {
-        is(0x0.U) {  //add, sub
+        is(0x0.U) {
           switch(funct7) {
-            is(0x00.U) {
-              reg(rd) := reg(rs1) + reg(rs2) //add
+            is(0x00.U) { //add
+              reg(rd) := reg(rs1) + reg(rs2)
             }
-            is(0x20.U) {
-              reg(rd) := reg(rs1) - reg(rs2) //sub
+            is(0x20.U) { //sub
+              reg(rd) := reg(rs1) - reg(rs2)
             }
           }
         }
-        is(0x6.U) {
-          reg(rd) := reg(rs1) | reg(rs2) //or
+        is(0x6.U) { //or
+          reg(rd) := reg(rs1) | reg(rs2)
         }
-        is(0x7.U) {
-          reg(rd) := reg(rs1) & reg(rs2) //and
+        is(0x7.U) { //and
+          reg(rd) := reg(rs1) & reg(rs2)
         }
-        is(0x1.U) {     //sll
+        is(0x1.U) { //sll
           shift := reg(rs2)
           reg(rd) := reg(rs1) << shift
         }
         is(0x5.U) {
           switch(funct7) {
-            is(0x00.U) {
-              reg(rd) := reg(rs1) >> reg(rs2) //srl
+            is(0x00.U) { //srl
+              reg(rd) := reg(rs1) >> reg(rs2)
             }
-            is(0x20.U) {
-              reg(rd) := reg(rs1) >> reg(rs2) //sra             ***** mangler msb-extend
+            is(0x20.U) { //sra
+              reg(rd) := (reg(rs1).asSInt >> reg(rs2)).asUInt()
             }
           }
         }
-        is(0x2.U) {
-          when(reg(rs1)(31) =/= reg(rs2)(31)){ //slt
+        is(0x2.U) { //slt
+          when(reg(rs1)(31) =/= reg(rs2)(31)){
             reg(rd) := reg(rs1) > reg(rs2)
           } otherwise {
             reg(rd) := reg(rs1) < reg(rs2)
           }
         }
-        is(0x3.U) {
-          reg(rd) := reg(rs1) < reg(rs2) //sltu
+        is(0x3.U) { //sltu
+          reg(rd) := reg(rs1) < reg(rs2)
         }
 
       }
     }
-    is(0x13.U) {                              //I-type
+    //________________________________________I-type_________________________
+    is(0x13.U) {
       switch(funct3) {
-        is(0x0.U) {
-          reg(rd) := reg(rs1) + imm //addi
+        is(0x0.U) { //addi
+          reg(rd) := reg(rs1) + imm
         }
-        is(0x4.U) {
-          reg(rd) := reg(rs1) ^ imm //xori
+        is(0x4.U) { //xori
+          reg(rd) := reg(rs1) ^ imm
         }
-        is(0x6.U) {
-          reg(rd) := reg(rs1) | imm //ori
+        is(0x6.U) { //ori
+          reg(rd) := reg(rs1) | imm
         }
-        is(0x7.U) {
-          reg(rd) := reg(rs1) & imm //andi
+        is(0x7.U) { //andi
+          reg(rd) := reg(rs1) & imm
         }
-        is(0x1.U) {
-          reg(rd) := reg(rs1) << imm(4, 0) //slli
+        is(0x1.U) { //slli
+          reg(rd) := reg(rs1) << imm(4, 0)
         }
         is(0x5.U) {
           switch(imm(11,5)) {
-            is(0x00.U) {
-              reg(rd) := reg(rs1) >> imm(4, 0) //srli
-            }
-            is(0x20.U) {                                      //***** mangler msb-extend
+            is(0x00.U) { //srli
               reg(rd) := reg(rs1) >> imm(4, 0)
-              when(reg(rs1)(31.U - imm(4,0))) {
-                //val bal = 31
-                reg(rd) := Cat(0xFFFFF.U, reg(rs1)(31,0)) //srai
-              } otherwise {
-                //reg(rd) := reg(rs1) >> imm(4, 0)
-              }
+            }
+            is(0x20.U) { //srai
+              reg(rd) := (reg(rs1).asSInt() >> imm(4, 0)).asUInt()
             }
           }
         }
-        is(0x2.U) {
-          when(reg(rs1)(31) =/= imm(31)){ //slti
+        is(0x2.U) { //slti
+          when(reg(rs1)(31) =/= imm(31)){
             reg(rd) := reg(rs1) > imm
           } otherwise {
             reg(rd) := reg(rs1) < imm
           }
         }
-        is(0x3.U) {
-          reg(rd) := reg(rs1) < imm //sltiu
+        is(0x3.U) { //sltiu
+          reg(rd) := reg(rs1) < imm
         }
       }
     }
-    is(0x03.U) { //I-type loads
+    //________________________________________I-type Loads_____________________
+    is(0x03.U) {
       switch(funct3) {
         is(0x0.U) { //lb
           reg(rd) := mem.read(readAddr)
-          when(mem.read(readAddr)(7)) { //sign-extend?
-           reg(rd) := ~reg(rd)
+          when(mem.read(readAddr)(7)) { //Check msb for sign
+           reg(rd) := ~reg(rd) //sign-extend
           }
         }
         is(0x1.U) { //lh
           reg(rd) := Cat(0x0000.U,mem.read(readAddr + 1.U), mem.read(readAddr))
-          when(mem.read(readAddr + 1.U)(7)) { //sign-extend
-            reg(rd) := ~reg(rd)
+          when(mem.read(readAddr + 1.U)(7)) { //Check msb for sign
+            reg(rd) := ~reg(rd) //sign-extend
           }
         }
         is(0x2.U) { //lw
-          reg(rd) := Cat(mem.read(readAddr + 3.U), mem.read(readAddr + 2.U), mem.read(readAddr + 1.U), mem.read(readAddr))
+          reg(rd) := Cat(mem.read(readAddr + 3.U),
+            mem.read(readAddr + 2.U),
+            mem.read(readAddr + 1.U),
+            mem.read(readAddr))
         }
         is(0x4.U) { //lbu
           reg(rd) := mem.read(readAddr)
@@ -200,7 +196,8 @@ class SingleCycleRiscV extends Module {
         }
       }
     }
-    is(0x23.U) { //S-type
+    //_______________________________________________S-type________________________
+    is(0x23.U) {
       switch(funct3) {
         is(0x0.U) { //sb
           mem.write(writeAddr, dataIn(7, 0))
@@ -217,7 +214,8 @@ class SingleCycleRiscV extends Module {
         }
       }
     }
-    is(0x63.U) { //B-type
+    //________________________________________________B-type__________________________
+    is(0x63.U) {
       switch(funct3) {
         is(0x0.U) { //beq
           when(reg(rs1) === reg(rs2)) {
@@ -263,6 +261,7 @@ class SingleCycleRiscV extends Module {
         }
       }
     }
+    //__________________________________J-type, U-type, ECALL and lui, auipc___________
     is(0x6F.U) { //J-type jal
       reg(rd) := pc + 4.U
       pc := pc + Jimm
@@ -277,14 +276,20 @@ class SingleCycleRiscV extends Module {
     is(0x17.U) { //U-type auipc
       reg(rd) := pc + Uimm
     }
+    is(0x73.U) { //ecall
+      switch(reg(10)) {
+        is(10.U) {
+          io.done := true.B
+        }
+      }
+    }
   }
 
-  // done should be set when the program ends, and the tester shall stop
-  io.done := true.B
+  reg(0) := 0.U //x0 should always be zero
 
-  // Make the register file visible to the tester
-  //for (i <- 0 until 32) io.regDeb(i) := reg(i)
+  // Make the io's file visible to the tester
   io.pcDeb <> pc
   io.regDeb <> reg
-  io.memDeb := Cat(mem.read(8.U), mem.read(9.U), mem.read(10.U), mem.read(11.U))
+  for (i <- program.indices) io.imemDeb(i) := imem(i)
+  //io.memDeb := Cat(mem.read(8.U), mem.read(9.U), mem.read(10.U), mem.read(11.U))
 }
--- finasgmt/src/test/scala/SingleCycleRiscVTester.scala
+++ finasgmt/src/test/scala/SingleCycleRiscVTester.scala
@@ -1,5 +1,6 @@
 import chisel3.iotesters._
 import org.scalatest._
+import scala.util.control.Breaks._
 
 class RiscVSpec extends FlatSpec with Matchers {
   "RiscV" should "pass" in {
@@ -7,22 +8,27 @@ class RiscVSpec extends FlatSpec with Matchers {
       new PeekPokeTester(c) {
 
         def printReg() = {
+          //for (i <- 0 until 32) {
+          //  print("x" + i + "\t")
+          //}
+          //println()
           for (i <- 0 until 32) {
-            print("x" + i + "\t")
-          }
-          println()
-          for (i <- 0 until 32) {
-            print(Integer.toHexString(peek(dut.io.regDeb(i)).toInt) + "\t")
+            print("x" + i + ": " + Integer.toHexString(peek(dut.io.regDeb(i)).toInt) + "\t")
           }
           println()
         }
 
-        for (i <- 0 until 109) {
-          println(peek(dut.io.pcDeb) + ": 0x" + Integer.toHexString(peek(dut.io.imemDeb(peek(dut.io.pcDeb).toInt / 4)).toInt))
+        var i = 0
+
+        do {
+          //if (i > 1245) println("PC = " + peek(dut.io.pcDeb) + ": 0x" + Integer.toHexString(peek(dut.io.imemDeb(peek(dut.io.pcDeb).toInt / 4)).toInt))
           step(1)
-          printReg()
-          //println("Memory: " + Integer.toHexString(peek(dut.io.memDeb).toInt))
+          i += 1
+          //if (i > 1245) printReg()
         }
+        while(peek(dut.io.done) == 0 && i < 3000)
+        printReg()
+        //1245 cycles to finish L7
       }
     }
   }
